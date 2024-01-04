b48eae32e498d96ded98687eb7e270b0d9d739f4
kezdetphia <fehermark88@gmail.com>
2024-01-03 19:36:34 -0800

    add searchbar with google places autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,38 @@
 import React from "react";
-
-import { Image,SafeAreaView,Text,View } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <View className='p-5'>
+      <View className="p-5">
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
 
-      <Image
-      style={{width:100, height:100, resizeMode:'contain'}}
-      source={{
-        uri: "https://links.papareact.com/gzs",
-      }}
-      />
-      <NavOptions/>
+        <GooglePlacesAutocomplete
+            placeholder="Where From?"
+          styles={{
+            container: { flex: 0 },
+            textInput:{
+              fontSize:18
+            }
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          nearbyPlacesAPI="GooglePacesSearch"
+          debounce={400}
+        />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
 }
-
-
