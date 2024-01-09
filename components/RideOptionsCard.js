c25b77a411c7dc64bc336c76801ec31c0a8095dd
kezdetphia <fehermark88@gmail.com>
2024-01-08 18:14:15 -0800

    rideoptions fix

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -40,59 +40,71 @@ const SURGE_CHARGE_RATE = 1.5
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
-  const travelTImeInformation = useSelector(selectTravelTimeInformation)
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
+  const renderItem = ({ item }) => {
+    const { id, title, multiplier, image } = item;
+    const selectedClass = id === selected?.id ? "bg-gray-200" : "";
+
+    return (
+      <TouchableOpacity
+        onPress={() => setSelected(item)}
+        className={`flex-row items-center py-2 px-4 justify-between ${selectedClass}`}
+      >
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{ uri: image }}
+        />
+        <View className="-ml-6">
+          <Text className="text-lg font-semibold">{title}</Text>
+          <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
+        </View>
+        <Text className="text-xl">
+          {new Intl.NumberFormat("en-us", {
+            style: "currency",
+            currency: "USD",
+          }).format(
+            (travelTimeInformation?.duration?.value *
+              SURGE_CHARGE_RATE *
+              multiplier) /
+              100
+          )}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <SafeAreaView className="bg-white flex-grow">
-      <View className="flex flex-row items-center p-4 ">
-        <TouchableOpacity
-          onPress={() => navigation.navigate("NavigateCard")}
-          className=" "
-        >
-          <Icon name="chevron-left" type="fontawesome " />
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-row items-center p-4">
+        <TouchableOpacity onPress={() => navigation.navigate("NavigateCard")}>
+          <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <View className="flex-1 justify-center items-center">
-          <Text className="text-center text-xl">Select a Ride - {travelTImeInformation?.distance?.text}</Text>
+          <Text className="text-xl">
+            Select a Ride - {travelTimeInformation?.distance?.text}
+          </Text>
         </View>
       </View>
-      <ScrollView>
 
       <FlatList
         data={data}
-        keyExtractor={(item)=> item.id}
-        renderItem={({ item: {id,title,multiplier,image}, item }) => (
-          <TouchableOpacity
-          onPress={() => setSelected(item)}
-          className={`flex-row items-center py-auto justify-between px-3 ${id === selected?.id && 'bg-gray-200'} `}
-          >
-            <Image
-              style={{
-                width: 100,
-                height: 100,
-                resizeMode: "contain",
-              }}
-              source={{ uri: image }}
-              />
-            <View className="-ml-6">
-              <Text className="text-lg font-semibold">{title}</Text>
-              <Text>{travelTImeInformation?.duration?.text} Travel Time</Text>
-            </View>
-            <Text className="text-xl">
-              {new Intl.NumberFormat('en-us',{
-                style: 'currency',
-                currency:'USD'
-              }).format(
-                (travelTImeInformation?.duration?.value * SURGE_CHARGE_RATE * multiplier) / 100 
-                )
-              }
-            </Text>
-          </TouchableOpacity>
-        )}
-        />
-        </ScrollView>
-      <View className='mt-auto border-t border-gray-200'>
-        <TouchableOpacity disabled={!selected} className={`bg-black py-3 m-3 ${!selected && 'bg-gray-300'}` }>
-          <Text className='text-center text-white text-xl'>Choose {selected?.title ? selected.title : 'a car'}  </Text>
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+
+      <View className="border-t border-gray-200">
+        <TouchableOpacity
+          disabled={!selected}
+          className={`py-3 m-3 ${!selected && "bg-gray-300"} bg-black`}
+        >
+          <Text className="text-center text-white text-xl">
+            Choose {selected?.title ? selected.title : "a car"}
+          </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
